2cf468ef66e39934ad60e9e76033f6ca788afc2d
Hakkari <pykurwe@gmail.com>
2024-03-04 23:41:56 +0400

    added classes for styling

--- tasklist/src/routes/EditTask.jsx
+++ tasklist/src/routes/EditTask.jsx
@@ -1,4 +1,5 @@
-// Import the necessary hook from react-router-dom
+import "../styles/EditTask.css"
+
 import { useParams, Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react"
 
@@ -29,40 +30,45 @@ const EditTask = () => {
         const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
         const updatedTasks = tasks.map(t => (t.id === task.id ? editedTask : t));
         localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-        setOriginalTask(editedTask); // Обновляем оригинальные данные
-        navigate("/"); // Перенаправляем обратно к списку задач
+        setOriginalTask(editedTask);
+        navigate("/");
     };
 
-    if (!task) {
-        return <div>Loading...</div>;
+    if(!task) {
+        return <div>Loading...</div>
     }
 
     return (
-        <div>
-            <h1>Edit Task {task.id}</h1>
-            <p>Original Title: {originalTask.title}</p>
-            <p>Original Description: {originalTask.description}</p>
-            <form onSubmit={handleSave}>
-                <div>
-                    <label>Title:</label>
-                    <input 
-                        type="text" 
-                        name="title" 
-                        value={editedTask.title}
-                        onChange={handleInputChange} 
-                    />
-                </div>
-                <div>
-                    <label>Description:</label>
-                    <textarea 
-                        name="description"
-                        value={editedTask.description}
-                        onChange={handleInputChange} 
-                    />
-                </div>
-                <button type="submit">Save</button>
-            </form>
-            <Link to="/">Back to Tasks</Link>
+        <div className="edit-task-menu">
+            <h1>Edit Task #{task.id}</h1>
+            <div className="edit-task-menu__taskInfo">
+                <p>Task Name: {originalTask.title}</p>
+                <p className="PrevTaskDesc">Description: <span>{originalTask.description}</span></p>
+            </div>
+            <div className="edit-task-menu__taskChange">
+                <form onSubmit={handleSave}>
+                    <div className="edit-task-menu__taskChange taskName">
+                        <label>Title:</label>
+                        <input 
+                            type="text" 
+                            name="title" 
+                            value={editedTask.title}
+                            onChange={handleInputChange} 
+                        />
+                    </div>
+                    <div className="edit-task-menu__taskChange taskDesc">
+                        <label>Description:</label>
+                        <textarea 
+                            name="description"
+                            value={editedTask.description}
+                            onChange={handleInputChange} 
+                        />
+                    </div>
+                    <button type="submit">Save</button>
+                </form>
+                <button className="returnToTasks"><Link className="reactLink" to="/">Back to Tasks</Link></button>
+            </div>
+            
         </div>
     );
 }
